0014d9b431678b851c41ebee4c6d8575081542bc
Henna Anttonen <hanttone@gmail.com>
2020-10-22 18:42:58 +0200

    local storage complete and working

--- index.js
+++ index.js
@@ -70,4 +70,39 @@ function get (selector) {
   return document.querySelector(selector);
 }
 
+//LOCALSTORAGE
+
+const form = document.querySelector('form');
+
+const localStorageListLoaded = loadFromStorageList('Motto and Notes')
+
+let localStorageList = localStorageListLoaded || [];
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const motto = form.motto.value;
+    const notes = form.notes.value;
+    addToStorageList(motto, notes);
+    form.reset()
+});
+
+
+function addToStorageList(motto, notes) {
+    const journalEntry = {motto: motto, notes: notes};
+    localStorageList = [...localStorageList, journalEntry];
+    saveToStorageList(localStorageList)
+}
+
+function saveToStorageList(input) {
+    localStorage.setItem('Motto and Notes', JSON.stringify(input));
+}
+
+function loadFromStorageList(name) {
+    try {
+     return JSON.parse(localStorage.getItem(name));
+   } catch(error) {
+     console.log(error.message);
+   }
+ }
+
   
