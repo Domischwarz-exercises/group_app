ff77ec6f531acc3f95d9d4ca49e11bdb3d0c6530
Henna Anttonen <hanttone@gmail.com>
2020-10-25 18:49:42 +0100

    getting text for journal

--- index.js
+++ index.js
@@ -249,142 +249,34 @@ function journalsSection(inputAPI, index) {
   <h3 class="card__date card-grid">YESTERDAY</h3>
   <h4 class="card__startitle card-grid card-title">Rating:</h4>
   <div class="card__starcontainer">
-  <img class="card__star--img test" alt="Black star"/>
-  <img class="card__star--img test" alt="Black star"/>
-  <img class="card__star--img test" alt="Black star"/>
-  <img class="card__star--img test" alt="Black star"/>
-  <img class="card__star--img test" alt="Black star"/>
+    <img class="card__star--img" src="img/starBlue.png" alt="Black star" />
+    <img class="card__star--img" src="img/starBlue.png" alt="Black star" />
+    
+    <img class="card__star--img" src="img/starBlue.png" alt="Black star" />
+    <img class="card__star--img" src="img/starGrey.png" alt="Grey star" />
+    <img class="card__star--img" src="img/starGrey.png" alt="Grey star" />
+  </div>
+
+<h4 class="card__rectangletitle card-grid card-title">Comprehension:</h4>
+<div class="card__rectanglecontainer card-grid ">
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleBlue.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleGrey.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleGrey.png" alt="" />
+  <img class="card__rectangle--img" src="img/rectangleGrey.png" alt="" />
+</div>
+<h4 class="card__mottotitle card-grid card-title">Motto:</h4>
+<h3 class="card__motto card-grid ">${inputAPI.motto}</h3>
+<h4 class="card__notetitle card-grid card-title">Notes:</h4>
+<p class="card__note card-grid ">${inputAPI.notes}
+</p>
   `
-
-  const stars = getAll('.test')
-  starContainer.forEach(star => starBlue(star, inputAPI));
-
-  function starBlue(images, apiInput) {
-    images.src = "img/starGrey.png";
-    console.log(images)
-    for (let i = 0; i <= apiInput.rating; i++) {
-      console.log(apiInput.rating)
-      images[i].src = "img/starBlue.png"
-    }
-  }
 }
-
-  
-  
-
- /* starContainer.forEach((img, index) =>{
-    img.addEventListener("click", () => {
-      starContainer.forEach(star => star.src = "img/starGrey.png");
-      for (let i = 0; i <= index; i++){
-        starContainer[i].src="img/starBlue.png"; 
-      }
-    })
-  })*/
-
-
-
-  /*rectangleContainer.forEach(rectangle => rectangle.src = "img/rectangleGrey.png");
-    for (let i = 0; i <= index; i++){
-      rectangleContainer[i].src="img/rectangleBlue.png"; 
-    }
-  })*/
-
-  /*<div class="card">
-          <h3 class="card__date card-grid">YESTERDAY</h3>
-          <h4 class="card__startitle card-grid card-title">Rating:</h4>
-          <div class="card__starcontainer">
-            <img
-              class="card__star--imgblue test__one"
-              src="img/starBlue.png"
-              alt="Black star"
-            />
-            <img
-              class="card__star--imgblue"
-              src="img/starBlue.png"
-              alt="Black star"
-            />
-            <img
-              class="card__star--imgblue"
-              src="img/starBlue.png"
-              alt="Black star"
-            />
-            <img
-              class="card__star--imggrey"
-              src="img/starGrey.png"
-              alt="Grey star"
-            />
-            <img
-              class="card__star--imggrey"
-              src="img/starGrey.png"
-              alt="Grey star"
-            />
-          </div>
-
-          <h4 class="card__rectangletitle card-grid card-title">
-            Comprehension:
-          </h4>
-          <div class="card__rectanglecontainer card-grid">
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imgblue"
-              src="img/rectangleBlue.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imggrey"
-              src="img/rectangleGrey.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imggrey"
-              src="img/rectangleGrey.png"
-              alt=""
-            />
-            <img
-              class="card__rectangle--imggrey"
-              src="img/rectangleGrey.png"
-              alt=""
-            />
-          </div>
-          <h4 class="card__mottotitle card-grid card-title">Motto:</h4>
-          <h3 class="card__motto card-grid">"Thats life in the city"</h3>
-          <h4 class="card__notetitle card-grid card-title">Notes:</h4>
-          <p class="card__note card-grid">
-            Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si
-            mihi. Et quidem se repellere, idque instituit docere sic omne
-            animal, simul atque.
-          </p>
-        </div>*/
-
 
 //function html container creator
 function createContainer(target, className) {
